77ab5d69b84d86dabdbf46ebbf72400d1febf476
Daniel Lewis <daniel.d.lewis2@shell.com>
2025-05-19 11:57:36 +0100

    example enhanced

--- src/app/components/example-enhanced/example-enhanced.component.ts
+++ src/app/components/example-enhanced/example-enhanced.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core'
+import { AfterViewInit, Component, inject, Injector, OnInit, viewChild, ViewContainerRef } from '@angular/core'
+import { RemoteService } from '../../services/remote/remote.service'
 
 @Component({
   selector: 'app-example-enhanced',
@@ -6,4 +7,28 @@ import { Component } from '@angular/core'
   templateUrl: './example-enhanced.component.html',
   styleUrl: './example-enhanced.component.scss',
 })
-export class ExampleEnhancedComponent {}
+export class ExampleEnhancedComponent implements OnInit, AfterViewInit {
+  private vc = viewChild('vc', { read: ViewContainerRef })
+
+  private remoteService: RemoteService = inject(RemoteService)
+  private injector: Injector = inject(Injector)
+
+  ngOnInit() {
+    this.injectRemoteService().then()
+  }
+
+  ngAfterViewInit() {
+    this.createRemoteComponent().then()
+  }
+
+  private async injectRemoteService() {
+    const MyRemoteService = await this.remoteService.getFromMySharedLib('MyRemoteService')
+    const myRemoteService = this.injector.get(MyRemoteService)
+    myRemoteService.hello()
+  }
+
+  private async createRemoteComponent() {
+    const MyRemoteComponent = await this.remoteService.getFromMySharedLib('MyRemoteComponent')
+    this.vc()!.createComponent(MyRemoteComponent, { injector: this.injector })
+  }
+}
